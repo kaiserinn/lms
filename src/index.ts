60ec8236de0c22472bc846e9f1fa96bcfce04dfc
Azhar Rizqullah <azharizqullah90@gmail.com>
2024-11-25 23:21:57 +0800

    refactor: error handling

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 import { apiRouter } from "./routes/api";
+import { errorHandler } from "./lib/utils/errorHandler";
 
 const app = new Hono();
 
 app.use(logger());
 
-app.onError((err, c) => {
-    if (err instanceof HTTPException) {
-        return c.json({ error: err.message }, err.status);
-    }
-
-    if (err instanceof Error) {
-        if ("sqlState" in err && err.sqlState === "45000") {
-            return c.json({ error: err.message }, 400);
-        }
-    }
-
-    console.error(err);
-    return c.json({ error: "Internal server error." }, 500);
-});
+app.onError(errorHandler);
 
 app.route("/api", apiRouter);
 
---
+++ src/lib/utils/errorHandler.ts
@@ -0,0 +1,28 @@
+import type { ErrorHandler } from "hono";
+import { HTTPException } from "hono/http-exception";
+import type { StatusCode } from "hono/utils/http-status";
+
+const sqlStateToHttpStatus: Record<string, StatusCode> = {
+    "45000": 400, // Bad Request
+    "45401": 401, // Unauthorized
+    "45403": 403, // Forbidden
+    "45404": 404, // Not Found
+};
+
+export const errorHandler: ErrorHandler = (err, c) => {
+    if (err instanceof HTTPException) {
+        return c.json({ error: err.message }, err.status);
+    }
+
+    if ("sqlState" in err) {
+        const sqlState = err.sqlState as string;
+
+        if (sqlState.startsWith("45")) {
+            const status = sqlStateToHttpStatus[sqlState] || 400;
+            return c.json({ error: err.message }, status);
+        }
+    }
+
+    console.error(err);
+    return c.json({ error: "Internal server error." }, 500);
+}
