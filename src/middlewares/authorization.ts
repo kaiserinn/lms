8a32cae47fd40112ade87ed2338f8b05e532d341
Azhar Rizqullah <azharizqullah90@gmail.com>
2024-11-28 20:12:42 +0800

    feat: authorization middlewares

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,27 @@
+import { db } from "@/db";
+import type { MiddlewareHandler } from "hono";
+import { getCookie } from "hono/cookie";
+
+export const isAdmin: MiddlewareHandler = async (c, next) => {
+    const token = getCookie(c, "auth_session");
+    
+    await db.check_admin(token);
+
+    await next();
+}
+
+export const isInstructor: MiddlewareHandler = async (c, next) => {
+    const token = getCookie(c, "auth_session");
+    
+    await db.check_instructor(token);
+
+    await next();
+}
+
+export const isStudent: MiddlewareHandler = async (c, next) => {
+    const token = getCookie(c, "auth_session");
+    
+    await db.check_student(token);
+
+    await next();
+}
