d592129c164c39d8a3ac43bf28f1e6f18fcf5ed1
Azhar Rizqullah <azharizqullah90@gmail.com>
2024-11-30 22:03:44 +0800

    feat: accounts endpoints

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
 import { authRouter } from "./auth";
+import { validateSession } from "@/middlewares/validateSession";
+import { accountsRouter } from "./accounts";
 
 const router = new Hono();
 
 router.route("/auth", authRouter);
+router.use(validateSession);
+router.route("/accounts", accountsRouter);
 
 export { router as apiRouter };
